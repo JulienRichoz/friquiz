9051fd2328999e1a0facde07df9396c12a241330
Julien Richoz <julienrichoz@outlook.com>
2023-11-30 16:47:12 +0100

    Creation migration user score

--- priv/repo/migrations/20231130152926_create_user_scores.exs
+++ priv/repo/migrations/20231130152926_create_user_scores.exs
@@ -2,6 +2,14 @@ defmodule Friquiz.Repo.Migrations.CreateUserScores do
   use Ecto.Migration
 
   def change do
+    create table(:user_scores, primary_key: false) do
+      add :id, :uuid, primary_key: true
+      add :score, :integer
 
+      add :user_id, references(:users, type: :uuid, on_delete: :nothing)
+      add :quiz_session_id, references(:quiz_sessions, type: :uuid, on_delete: :nothing)
+
+      timestamps()
+    end
   end
 end

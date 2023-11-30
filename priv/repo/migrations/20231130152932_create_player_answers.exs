ec7d370a62746199347abf6ecde7f4530a085929
Julien Richoz <julienrichoz@outlook.com>
2023-11-30 16:47:46 +0100

    Creation migration player answers

--- priv/repo/migrations/20231130152932_create_player_answers.exs
+++ priv/repo/migrations/20231130152932_create_player_answers.exs
@@ -2,6 +2,17 @@ defmodule Friquiz.Repo.Migrations.CreatePlayerAnswers do
   use Ecto.Migration
 
   def change do
+    create table(:player_answers, primary_key: false) do
+      add :id, :uuid, primary_key: true
+      add :givenAnswer, :string
+      add :isCorrect, :boolean
+      add :timestamp, :naive_datetime
 
+      add :user_id, references(:users, type: :uuid, on_delete: :nothing)
+      add :question_id, references(:questions, type: :uuid, on_delete: :nothing)
+      add :quiz_session_id, references(:quiz_sessions, type: :uuid, on_delete: :nothing)
+
+      timestamps()
+    end
   end
 end
